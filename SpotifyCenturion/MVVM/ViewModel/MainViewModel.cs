f555be7311b712cdaadf4af993ecc5e152503850
Pasante5 desa <p5@rgtdom.local>
2022-08-08 10:37:29 -0500

    I have reached the end

--- SpotifyCenturion/MVVM/ViewModel/MainViewModel.cs
+++ SpotifyCenturion/MVVM/ViewModel/MainViewModel.cs
@@ -9,7 +9,13 @@ namespace SpotifyCenturion.MVVM.ViewModel
 {
     class MainViewModel : ObservableObject
     {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand DiscoveryViewCommand { get; set; }
+
+
         public HomeViewModel HomeVM { get; set; }
+        public DiscoveryViewModel DiscoveryMV { get; set; }
+
         private object _currentView;
         public object CurrentView 
         {
@@ -23,7 +29,18 @@ namespace SpotifyCenturion.MVVM.ViewModel
         public MainViewModel()
         {
             HomeVM = new HomeViewModel();
+            DiscoveryMV = new DiscoveryViewModel();
             CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+
+            DiscoveryViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = DiscoveryMV;
+            });
         }
     }
 }
